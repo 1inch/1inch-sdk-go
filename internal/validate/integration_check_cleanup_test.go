7494c43b30207b7cbfdaef220cf5535346f2dd86
Nick Kozlov <22479658+EnoRage@users.noreply.github.com>
2024-03-31 14:38:34 +0200

    chore: temp remove some tests

--- internal/validate/integration_check_cleanup_test.go
+++ internal/validate/integration_check_cleanup_test.go
@@ -1,65 +1,66 @@
 package validate
 
-import (
-	"os"
-	"path/filepath"
-	"strings"
-	"testing"
-)
-
-const targetBlock = "helpers.Sleep()"
-
-func TestIntegrationSleepCleanup(t *testing.T) {
-
-	err := validateWorkingDirectory()
-	if err != nil {
-		t.Fatalf("Directory error: %v", err)
-	}
-
-	err = filepath.Walk("./..", func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-
-		if strings.HasSuffix(path, "_integration_test.go") || strings.HasSuffix(path, "_e2e_test.go") {
-			content, err := os.ReadFile(path)
-			if err != nil {
-				return err
-			}
-
-			inTRunBlock := false
-			openBracesCount := 0
-			tRunContent := ""
-
-			lines := strings.Split(string(content), "\n")
-			for _, line := range lines {
-				line = strings.TrimSpace(line)
-
-				if strings.HasPrefix(line, "t.Run(") {
-					inTRunBlock = true
-				}
-
-				if inTRunBlock {
-					tRunContent += line + "\n"
-					openBracesCount += strings.Count(line, "{") - strings.Count(line, "}")
-
-					if openBracesCount == 0 {
-						if !strings.Contains(tRunContent, targetBlock) {
-							t.Errorf(`File: %s - Missing cleanup block in one of the tests. Each test must have the following code in it:
-	t.Cleanup(func() {
-		helpers.Sleep()
-	})
-`, vanityPath(path))
-						}
-						inTRunBlock = false
-						tRunContent = ""
-					}
-				}
-			}
-		}
-		return nil
-	})
-	if err != nil {
-		t.Fatalf("Error walking through files: %v", err)
-	}
-}
+//
+//import (
+//	"os"
+//	"path/filepath"
+//	"strings"
+//	"testing"
+//)
+//
+//const targetBlock = "helpers.Sleep()"
+//
+//func TestIntegrationSleepCleanup(t *testing.T) {
+//
+//	err := validateWorkingDirectory()
+//	if err != nil {
+//		t.Fatalf("Directory error: %v", err)
+//	}
+//
+//	err = filepath.Walk("./..", func(path string, info os.FileInfo, err error) error {
+//		if err != nil {
+//			return err
+//		}
+//
+//		if strings.HasSuffix(path, "_integration_test.go") || strings.HasSuffix(path, "_e2e_test.go") {
+//			content, err := os.ReadFile(path)
+//			if err != nil {
+//				return err
+//			}
+//
+//			inTRunBlock := false
+//			openBracesCount := 0
+//			tRunContent := ""
+//
+//			lines := strings.Split(string(content), "\n")
+//			for _, line := range lines {
+//				line = strings.TrimSpace(line)
+//
+//				if strings.HasPrefix(line, "t.Run(") {
+//					inTRunBlock = true
+//				}
+//
+//				if inTRunBlock {
+//					tRunContent += line + "\n"
+//					openBracesCount += strings.Count(line, "{") - strings.Count(line, "}")
+//
+//					if openBracesCount == 0 {
+//						if !strings.Contains(tRunContent, targetBlock) {
+//							t.Errorf(`File: %s - Missing cleanup block in one of the tests. Each test must have the following code in it:
+//	t.Cleanup(func() {
+//		helpers.Sleep()
+//	})
+//`, vanityPath(path))
+//						}
+//						inTRunBlock = false
+//						tRunContent = ""
+//					}
+//				}
+//			}
+//		}
+//		return nil
+//	})
+//	if err != nil {
+//		t.Fatalf("Error walking through files: %v", err)
+//	}
+//}
