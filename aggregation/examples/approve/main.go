72035cd5b76dc70607a5d35f3b0147ec28e55217
Nick Kozlov <22479658+EnoRage@users.noreply.github.com>
2024-04-10 13:18:35 +0200

    feat: add example approve

---
+++ aggregation/examples/approve/main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"context"
+	"encoding/hex"
+	"math/big"
+	"os"
+
+	"github.com/ethereum/go-ethereum/common"
+
+	"github.com/1inch/1inch-sdk-go/aggregation"
+	"github.com/1inch/1inch-sdk-go/aggregation/models"
+	"github.com/1inch/1inch-sdk-go/constants"
+)
+
+/*
+This example demonstrates how to swap tokens on the PolygonChainId network using the 1inch SDK.
+The only thing you need to provide is your wallet address, wallet key, and dev portal token.
+This can be done through your environment, or you can directly set them in the variables below
+*/
+
+var (
+	privateKey     = os.Getenv("WALLET_KEY")
+	nodeUrl        = os.Getenv("NODE_URL")
+	devPortalToken = os.Getenv("DEV_PORTAL_TOKEN")
+)
+
+const (
+	PolygonDai  = "0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063"
+	PolygonWeth = "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619"
+)
+
+func main() {
+	config, err := aggregation.NewDefaultConfiguration(nodeUrl, privateKey, constants.EthereumChainId, "https://api.1inch.dev", devPortalToken)
+	if err != nil {
+		return
+	}
+	client, err := aggregation.NewClient(config)
+	if err != nil {
+		panic(err)
+		return
+	}
+	ctx := context.Background()
+
+	amountToSwap := big.NewInt(1e18)
+
+	allowanceData, err := client.GetApproveAllowance(ctx, models.ApproveAllowanceParams{
+		ChainId: constants.EthereumChainId,
+		ApproveControllerGetAllowanceParams: models.ApproveControllerGetAllowanceParams{
+			TokenAddress:  PolygonDai,
+			WalletAddress: client.Wallet.Address().Hex(),
+		},
+	})
+
+	allowance := new(big.Int)
+	allowance.SetString(allowanceData.Allowance, 10)
+
+	cmp := amountToSwap.Cmp(allowance)
+
+	if cmp > 0 {
+		//spender, err := client.GetApproveSpender(ctx, models.ApproveSpenderParams{ChainId: constants.EthereumChainId})
+		//if err != nil {
+		//	panic(err)
+		//	return
+		//}
+
+		approveData, err := client.GetApproveTransaction(ctx, models.ApproveTransactionParams{
+			ChainId: constants.EthereumChainId,
+			ApproveControllerGetCallDataParams: models.ApproveControllerGetCallDataParams{
+				TokenAddress: PolygonDai,
+				Amount:       amountToSwap.String(),
+			},
+		})
+		if err != nil {
+			panic(err)
+			return
+		}
+		data, err := hex.DecodeString(approveData.Data[2:])
+		if err != nil {
+			return
+		}
+
+		to := common.HexToAddress(approveData.Data)
+
+		tx, err := client.TxBuilder.New().SetData(data).SetTo(&to).Build(ctx)
+		if err != nil {
+			return
+		}
+
+		signedTx, err := client.Wallet.Sign(tx)
+		if err != nil {
+			return
+		}
+
+		err = client.Wallet.BroadcastTransaction(ctx, signedTx)
+		if err != nil {
+			return
+		}
+	}
+
+}
