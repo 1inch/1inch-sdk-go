c41b6afda6c36a4185b2d1553888f16eeaddec45
Nick Kozlov <22479658+EnoRage@users.noreply.github.com>
2024-04-29 15:34:27 +0200

    test: add new client test

--- aggregation/client_test.go
+++ aggregation/client_test.go
@@ -2,7 +2,9 @@ package aggregation
 
 import (
 	"context"
+	"fmt"
 	"math/big"
+	"reflect"
 	"testing"
 
 	gethCommon "github.com/ethereum/go-ethereum/common"
@@ -16,11 +18,10 @@ import (
 
 func TestNewClient(t *testing.T) {
 	mockAPI := api{
-		chainId:      1,
-		httpExecutor: nil,
+		chainId: 1,
 	}
 
-	mockWallet := NewMyWallet(gethCommon.HexToAddress("0x0000000000000000000000000000000000000000"), big.NewInt(constants.EthereumChainId))
+	mockWallet := NewMyWallet(gethCommon.HexToAddress("0x2c9b2dbdba8a9c969ac24153f5c1c23cb0e63914"), big.NewInt(constants.EthereumChainId))
 	mockTxBuilder := transaction_builder.NewFactory(mockWallet)
 
 	cfg := &Configuration{
@@ -52,6 +53,45 @@ func TestNewClient(t *testing.T) {
 	assert.Equal(t, mockAPI, clientOnlyAPI.api)
 	assert.Nil(t, clientOnlyAPI.Wallet)
 	assert.Nil(t, clientOnlyAPI.TxBuilder)
+}
+
+func TestClient(t *testing.T) {
+	mockAPI := api{
+		chainId: 1,
+		httpExecutor: &mockHttpExecutor{
+			ResponseObj: mockedSwapHttpApiResp,
+		},
+	}
+
+	mockWallet := NewMyWallet(gethCommon.HexToAddress("0x2c9b2dbdba8a9c969ac24153f5c1c23cb0e63914"), big.NewInt(constants.EthereumChainId))
+	mockTxBuilder := transaction_builder.NewFactory(mockWallet)
+
+	cfg := &Configuration{
+		APIConfiguration: &ConfigurationAPI{
+			API: mockAPI,
+		},
+		WalletConfiguration: &ConfigurationWallet{
+			Wallet:    mockWallet,
+			TxBuilder: mockTxBuilder,
+		},
+	}
+
+	client, err := NewClient(cfg)
+	assert.NoError(t, err)
+	assert.NotNil(t, client)
+
+	swapData, err := client.GetSwap(context.Background(), GetSwapParams{
+		Src:               "0x5a98fcbea516cf06857215779fd812ca3bef1b32",
+		Dst:               "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
+		Amount:            "10000",
+		Slippage:          1,
+		From:              "0x083fc10ce7e97cafbae0fe332a9c4384c5f54e45",
+		IncludeTokensInfo: true,
+		IncludeGas:        true,
+		IncludeProtocols:  true,
+	})
+	assert.NoError(t, err)
+	assert.NotNil(t, swapData)
 }
 
 type MyWallet struct {
@@ -135,4 +175,72 @@ func (w *MyWallet) IsEIP1559Applicable() bool {
 
 func (w *MyWallet) ChainId() int64 {
 	return w.chainID.Int64()
+}
+
+type mockHttpExecutor struct {
+	Called      bool
+	ExecuteErr  error
+	ResponseObj interface{}
+}
+
+func (m *mockHttpExecutor) ExecuteRequest(ctx context.Context, payload common.RequestPayload, v interface{}) error {
+	m.Called = true
+	if m.ExecuteErr != nil {
+		return m.ExecuteErr
+	}
+
+	// Copy the mock response object to v
+	if m.ResponseObj != nil && v != nil {
+		rv := reflect.ValueOf(v)
+		if rv.Kind() != reflect.Ptr || rv.IsNil() {
+			return fmt.Errorf("v must be a non-nil pointer")
+		}
+		reflect.Indirect(rv).Set(reflect.ValueOf(m.ResponseObj))
+	}
+	return nil
+}
+
+var mockedSwapHttpApiResp = SwapResponse{
+	FromToken: &TokenInfo{
+		Address:  "0x5a98fcbea516cf06857215779fd812ca3bef1b32",
+		Symbol:   "LDO",
+		Name:     "Lido DAO Token",
+		Decimals: 18,
+		LogoURI:  "https://tokens.1inch.io/0x5a98fcbea516cf06857215779fd812ca3bef1b32.png",
+		Tags: []string{
+			"tokens",
+		},
+	},
+	ToAmount: "6",
+	ToToken: &TokenInfo{
+		Address:  "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
+		Symbol:   "WETH",
+		Name:     "Wrapped Ether",
+		Decimals: 18,
+		LogoURI:  "https://tokens.1inch.io/0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2.png",
+		Tags: []string{
+			"PEG:ETH",
+			"tokens",
+		},
+	},
+	Protocols: [][][]SelectedProtocol{
+		{
+			{
+				{
+					FromTokenAddress: "0x5a98fcbea516cf06857215779fd812ca3bef1b32",
+					Name:             "SUSHI",
+					Part:             100,
+					ToTokenAddress:   "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
+				},
+			},
+		},
+	},
+	Tx: TransactionData{
+		Data:     "0x0502b1c50000000000000000000000005a98fcbea516cf06857215779fd812ca3bef1b32000000000000000000000000000000000000000000000000000000000000271000000000000000000000000000000000000000000000000000000000000000050000000000000000000000000000000000000000000000000000000000000080000000000000000000000000000000000000000000000000000000000000000100000000000000003b6d0340c558f600b34a5f69dd2f0d06cb8a88d829b7420ade8bb62d",
+		From:     "0x2c9b2dbdba8a9c969ac24153f5c1c23cb0e63914",
+		Gas:      257615,
+		GasPrice: "22800337026",
+		To:       "0x1111111254eeb25477b68fb85ed929f73a960582",
+		Value:    "0",
+	},
 }
