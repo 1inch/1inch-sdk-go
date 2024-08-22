4fac8d7280d9ec9625dab4c1030e74175dfd6566
Tanz0rz <tanner@1inch.io>
2024-08-22 15:56:07 -0600

    Updating readme to match latest code (#78)

--- sdk-clients/aggregation/models.go
+++ sdk-clients/aggregation/models.go
@@ -9,15 +9,15 @@ import (
 type SwapResponseExtended struct {
 	SwapResponse
 
-	TxNormalized NormalizedTransactionData
+	TxNormalized NormalizedTransactionData `json:"txNormalized"`
 }
 
 type NormalizedTransactionData struct {
-	Data     []byte
-	Gas      uint64
-	GasPrice *big.Int
-	To       gethCommon.Address
-	Value    *big.Int
+	Data     []byte             `json:"data"`
+	Gas      uint64             `json:"gas"`
+	GasPrice *big.Int           `json:"gasPrice"`
+	To       gethCommon.Address `json:"to"`
+	Value    *big.Int           `json:"value"`
 }
 
 type ApproveCallDataResponseExtended struct {
